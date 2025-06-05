c024f3e6aeabc800fd1edde9e6e7e246fdbb3b95
Michael OConnor <michael@cpo2llc.com>
2025-06-05 13:46:37 -0500

    bootstrap work

--- election1/admins/form.py
+++ election1/admins/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField, BooleanField
-from wtforms.validators import Length, Email, InputRequired
+from wtforms.validators import Length, Email, InputRequired, DataRequired, EqualTo
 from election1.models import Admin_roles
 from wtforms_alchemy.fields import QuerySelectField
 
@@ -13,7 +13,9 @@ class UserForm(FlaskForm):
     user_firstname = StringField(label='firstname', validators=[Length(min=2, max=30), InputRequired()])
     user_lastname = StringField(label='lastname', validators=[Length(min=2, max=30), InputRequired()])
     user_so_name = StringField(label='user name', validators=[Length(min=2, max=30), InputRequired()])
-    user_pass = StringField(label='password', validators=[Length(min=2, max=30), InputRequired()])
+    user_pass = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        InputRequired()])
     # user_role = StringField(label='role',  validators=[Length(min=1, max=1), InputRequired()])
     id_admin_role = QuerySelectField(query_factory=admin_roles_query, label='admin role', get_label='admin_role_name')
 
