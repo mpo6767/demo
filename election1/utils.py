b962e3c049f53481c2c2e2c916bb458707bd97b5
Michael OConnor <michael@cpo2llc.com>
2025-08-09 11:31:40 -0500

    salting hash password

--- election1/utils.py
+++ election1/utils.py
@@ -1,8 +1,22 @@
 import secrets
+import hashlib
 
 from datetime import datetime
 from flask import session, current_app
 from flask_login import current_user
+
+def hash_password(password):
+    salt = secrets.token_hex(16)  # Generate a 16-byte random salt
+    salted_password = salt + password
+    hash_obj = hashlib.sha256(salted_password.encode())
+    password_hash = hash_obj.hexdigest()
+    return salt, password_hash
+
+
+def verify_password(stored_salt, stored_hash, input_password):
+    salted_input = stored_salt + input_password
+    hash_obj = hashlib.sha256(salted_input.encode())
+    return hash_obj.hexdigest() == stored_hash
 
 
 def unique_security_token():
@@ -26,6 +40,7 @@ def session_check():
     if 'last_activity' in session:
         now = datetime.now()
         last_activity = datetime.strptime(session['last_activity'], "%Y-%m-%d %H:%M:%S")
+        print('last activity is', last_activity)
         time_difference = now - last_activity
         time_difference_in_seconds = time_difference.total_seconds()
         if time_difference_in_seconds > idle_timeout:
